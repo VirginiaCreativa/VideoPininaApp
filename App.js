6b9c82b8414aaafa6fe33e1685ff5e8c209e4641
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-10-19 08:17:57 -0500

    Add Logo

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SvgUri from 'react-native-svg-uri';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <SvgUri
+          width="100"
+          height="100"
+          source={require('./assets/logo.svg')}
+        />
         <Text style={{color: '#fff'}}>Virginia Velásquez Soto</Text>
       </View>
     );
