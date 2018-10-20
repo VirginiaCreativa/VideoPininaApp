14eeff8e2816af71cf11fe32c64253edec4b11b8
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-10-20 11:54:35 -0500

    rename add fonts

--- src/components/common/fontload.js
+++ src/components/common/fontload.js
@@ -9,9 +9,17 @@ import { Text } from 'react-native';
 
   async componentDidMount() {
     await Font.loadAsync({
-      'FjallaOne': require('./../../../assets/fonts/FjallaOne-Regular.ttf'),
-      'SourceSans-Regular': require('./../../../assets/fonts/SourceSansPro-Regular.ttf'),
-      'SourceSans-Bold': require('./../../../assets/fonts/SourceSansPro-Bold.ttf'),
+      FjallaOne: require('./../../../assets/fonts/FjallaOne-Regular.ttf'),
+      SourceSansLightItalic: require('./../../../assets/fonts/SourceSansPro-LightItalic.ttf'),
+      SourceSansLight: require('./../../../assets/fonts/SourceSansPro-Light.ttf'),
+      SourceSansRegular: require('./../../../assets/fonts/SourceSansPro-Regular.ttf'),
+      SourceSansItalic: require('./../../../assets/fonts/SourceSansPro-Italic.ttf'),
+      SourceSansBold: require('./../../../assets/fonts/SourceSansPro-Bold.ttf'),
+      SourceSansBoldItalic: require('./../../../assets/fonts/SourceSansPro-BoldItalic.ttf'),
+      SourceSansSemiBold: require('./../../../assets/fonts/SourceSansPro-SemiBold.ttf'),
+      SourceSansSemiBoldItalic: require('./../../../assets/fonts/SourceSansPro-SemiBoldItalic.ttf'),
+      SourceSansBlack: require('./../../../assets/fonts/SourceSansPro-Black.ttf'),
+      SourceSansBlackItalic: require('./../../../assets/fonts/SourceSansPro-BlackItalic.ttf'),
     });
     this.setState({ fontLoaded: true });
   }
